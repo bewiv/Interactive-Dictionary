684449846d4470948e50f5fc487032dda82a13f2
bewiv <admarouane@gmail.com>
2018-04-21 17:27:37 +0100

    counting for non-existing word making the program letter case insensitive

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import json
 data = json.load(open("data.json"))
 def translate(word):
-    return data[word]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "the word doesn't exist, please double check it"
 
 word = input("Enter word: ")
 
