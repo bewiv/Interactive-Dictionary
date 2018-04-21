22c32d8050c7ca4475c6da856be967ef6c2f3f5f
bewiv <admarouane@gmail.com>
2018-04-21 21:33:59 +0100

    if user entered "texas" this will check for "Texas" as well. in case user enters words like USA or NATO

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
         yn = input("did you mean %s instead? Entrer Y for yes or N for no: " % get_close_matches(word, data.keys())[0])
         if yn == "Y" :
